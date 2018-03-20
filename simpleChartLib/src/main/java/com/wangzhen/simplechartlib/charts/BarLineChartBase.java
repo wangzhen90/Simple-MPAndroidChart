c31bc05b18e1ef360399c5072a9d42b3f28dcaf5
王振 <wangzhen@xiaoshouyi.com>
2018-03-20 18:47:57 +0800

    准备些坐标轴的Render

--- simpleChartLib/src/main/java/com/wangzhen/simplechartlib/charts/BarLineChartBase.java
+++ simpleChartLib/src/main/java/com/wangzhen/simplechartlib/charts/BarLineChartBase.java
@@ -3,6 +3,7 @@ package com.wangzhen.simplechartlib.charts;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import com.wangzhen.simplechartlib.component.YAxis;
 import com.wangzhen.simplechartlib.data.chartData.BarLineScatterCandleBubbleData;
 import com.wangzhen.simplechartlib.data.entry.Entry;
 import com.wangzhen.simplechartlib.interfaces.charts.BarLineScatterCandleBubbleDataProvider;
@@ -14,6 +15,45 @@ import com.wangzhen.simplechartlib.interfaces.dataSets.IBarLineScatterCandleBubb
 
 public abstract class BarLineChartBase<T extends BarLineScatterCandleBubbleData<? extends
         IBarLineScatterCandleBubbleDataSet<? extends Entry>>> extends Chart<T> implements BarLineScatterCandleBubbleDataProvider {
+
+    /**
+     * 绘制entity的个数
+     */
+    protected int mMaxVisibleCount = 100;
+    protected boolean mAutoScaleMinMaxEnable = false;
+
+    /**
+     * x,y 同时缩放
+     */
+    protected boolean mPinchZoomEnable = false;
+    /**
+     * 双击缩放
+     */
+    protected boolean mDoubleTapToZoomEnabled = true;
+
+    private boolean mDragXEnabled = true;
+    private boolean mDragYEnabled = true;
+
+    private boolean mScaleXEnabled = true;
+    private boolean mScaleYEnabled = true;
+
+    /**
+     * TODO 暂时不知道怎么用
+     */
+    protected boolean mClipValuesToContent = false;
+    /**
+     * 整个chart的padding
+     */
+    protected float mMinOffset = 15.f;
+
+    /**
+     * TODO 暂时不知道怎么用
+     */
+//    protected OnDrawListener mDrawListener;
+
+    protected YAxis mYAxis;
+
+
 
 
     public BarLineChartBase(Context context) {
@@ -27,6 +67,8 @@ public abstract class BarLineChartBase<T extends BarLineScatterCandleBubbleData<
     public BarLineChartBase(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+
 
     
 
