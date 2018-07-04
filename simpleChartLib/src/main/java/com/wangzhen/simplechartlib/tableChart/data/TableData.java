22c8a2fa03304fd54612dea31decab89eca7f100
王振 <wangzhen@xiaoshouyi.com>
2018-07-04 19:16:21 +0800

    wuyong

--- simpleChartLib/src/main/java/com/wangzhen/simplechartlib/tableChart/data/TableData.java
+++ simpleChartLib/src/main/java/com/wangzhen/simplechartlib/tableChart/data/TableData.java
@@ -1,5 +1,6 @@
 package com.wangzhen.simplechartlib.tableChart.data;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 /**
@@ -8,11 +9,65 @@ import java.util.List;
 
 public class TableData<T> {
 
+    private  T[][] data;
     public String tableName;
     private List<Column> columns;
     private List<Column> childColumns;
 
-    //TODO 1.[column][row] => [row][column]
+
     //TODO 2.解析转化后的数据，填充childColumns (参考 TableParser)
+
+    /**
+     * [column][row] => [row][column]
+     *
+     * @param rowArray
+     * @param <T>
+     * @return
+     */
+    public static <T> T[][] transformColumnArray(T[][] rowArray) {
+        T[][] columnArray = null;
+
+        T[] row = null;
+
+        if (rowArray != null) {
+            int maxLength = 0;
+
+            for (T[] childRow : rowArray) {
+                if (childRow != null && childRow.length > maxLength) {
+                    maxLength = childRow.length;
+                    row = childRow;
+                }
+            }
+
+            if (row != null) {
+
+                columnArray = (T[][]) Array.newInstance(row.getClass().getComponentType(), maxLength);
+
+                for (int i = 0; i < rowArray.length; i++) {
+
+                    for (int j = 0; j < rowArray[i].length; j++) {
+                        if (columnArray[j] == null) {
+                            columnArray[j] = (T[]) Array.newInstance(row.getClass().getComponentType(), rowArray.length);
+                        }
+                        columnArray[j][i] = rowArray[i][j];
+                    }
+                }
+            }
+        }
+        return columnArray;
+    }
+
+
+    public void setData(T[][] data) {
+        this.data = data;
+    }
+
+    public T[][] getData() {
+        return data;
+    }
+
+
+
+
 
 }
